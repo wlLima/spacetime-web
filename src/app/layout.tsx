fc4d4974014753924aa134f5e6d72c7c3b379a04
wlLima <lima14.wl@gmail.com>
2023-06-04 08:51:14 -0300

    fix: changed condition to check when no have token

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,9 +39,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2  rounded-full bg-purple-700 opacity-50 blur-full" />
             {/* Stripes */}
             <div className="absolute bottom-0 right-0 top-0 w-2 bg-stripes " />
-
-            {isAuthenticated?.value !== '' ? <Profile /> : <SignIn />}
-
+            {isAuthenticated?.value !== '' &&
+            cookies().has('token') === true ? (
+              <Profile />
+            ) : (
+              <SignIn />
+            )}
             <Hero />
             <Copyright />
           </div>
